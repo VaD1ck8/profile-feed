b2f4a28a9810c667c28800abd6849084f6e66f59
VaD1ck8 <shyd0497@gmail.com>
2026-06-27 07:59:40 +0300

    fix: add base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/profile-feed/",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
